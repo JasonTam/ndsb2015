d2c2855f15ebb0f43e81b517cb50058d66a8f884
jason <jasontam22@gmail.com>
2015-01-15 16:58:04 -0500

    rewrote some basical improc stuff. modualrized some

---
+++ feature/extract_feats.py
@@ -0,0 +1,34 @@
+__author__ = 'jason'
+
+import os
+import numpy as np
+from skimage.io import imread
+import feature.improc as improc
+from skimage.transform import resize
+
+
+def im_features(im_path):
+    im = imread(im_path, as_grey=True)
+    im_bw = improc.gray_to_bw(im)
+    labels = improc.bw_labels(im_bw)
+    region_max = improc.get_max_region(labels, im_bw)
+    axisratio = improc.get_minor_major_ratio(region_max)
+
+    maxPixel = 25
+    # imageSize = maxPixel * maxPixel
+    im = resize(im, (maxPixel, maxPixel))
+
+    # Store the rescaled image pixels and the axis ratio
+    im_flat = im.flatten()
+    x = np.concatenate((im_flat, [axisratio]))
+
+    return x
+
+
+if __name__ == '__main__':
+    f_dir = 'data/'
+    # f_name = '55511.jpg'
+    f_name = '116800.jpg'
+    f_path = os.path.join(f_dir, f_name)
+
+    features = im_features(f_path)
---
+++ feature/improc.py
@@ -0,0 +1,82 @@
+__author__ = 'jason'
+
+import os
+import numpy as np
+from skimage.io import imread
+from skimage import morphology
+from skimage.filter import threshold_otsu, rank
+from skimage import measure
+import matplotlib.pyplot as plt
+
+
+def get_max_region(label_list, imagethres):
+    regions = measure.regionprops(label_list)
+    region_max = None
+    for region in regions:
+        # check to see if the region is at least 50% nonzero
+        if sum(imagethres[label_list == region.label])*1.0/region.area < 0.50:
+            continue
+        if region_max is None:
+            region_max = region
+        if region_max.filled_area < region.filled_area:
+            region_max = region
+    return region_max
+
+
+def get_minor_major_ratio(region_prop):
+    # guard against cases where the segmentation fails by providing zeros
+    ratio = region_prop.minor_axis_length*1.0 / region_prop.major_axis_length \
+        if region_prop and region_prop.major_axis_length else 0.0
+    return ratio
+
+
+def gray_to_bw(image, method='mean', locality_radius=15):
+    image = image.copy()
+    # Create the thresholded image to eliminate some of the background
+    # todo: consider the use of Otsu's method
+    im_thresh = None
+    if method == 'mean':
+        im_thresh = np.where(image > np.mean(image), 0., 1.0)
+    elif method == 'global_otsu':
+        threshold_global_otsu = threshold_otsu(image)
+        im_thresh = image >= threshold_global_otsu
+    elif method == 'local_otsu':
+        locality_radius = 15
+        strel = morphology.disk(locality_radius)
+        local_otsu = rank.otsu(image, strel)
+        im_thresh = image >= local_otsu
+    else:
+        print 'Invalid threshold method'
+
+    return im_thresh
+
+
+def bw_labels(im_bw):
+    #Dilate the image
+    imdilated = morphology.dilation(im_bw, np.ones((4, 4)))
+
+    # Create the label list
+    # label dilated image so that close regions have the same label
+    label_list = measure.label(imdilated)
+    label_list = (im_bw*label_list).astype(int)
+
+    return label_list
+
+if __name__ == '__main__':
+    f_dir = 'data/'
+    # f_name = '55511.jpg'
+    f_name = '116800.jpg'
+    f_path = os.path.join(f_dir, f_name)
+    im = imread(f_path)
+    im_bw = gray_to_bw(im)
+    labels = bw_labels(im_bw)
+    region_max = get_max_region(labels, im_bw)
+    feat = get_minor_major_ratio(region_max)
+    print feat
+
+
+    plt.ion()
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(im, cmap='gray', interpolation='none')
+    ax2.imshow(region_max.image, cmap='gray', interpolation='none')
+    ax1.set_title(f_path)
