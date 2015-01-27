74ddb467b315f9f24628aa2568bde2d321c11d18
JasonTam <JasonTam22@gmail.com>
2015-01-26 21:29:51 -0500

    modified to grayscale

--- train_caffenet.sh
+++ train_caffenet.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
-
+cp ./train_val.prototxt ./models
+cp ./solver.prototxt ./models
 ~/documents/caffe/build/tools/caffe train \
     --solver=/afs/ee.cooper.edu/user/t/a/tam8/documents/ndsb2015/solver.prototxt
